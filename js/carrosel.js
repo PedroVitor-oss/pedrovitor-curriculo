1e9421f315aa2e3bf207ed9d243141b32c1c57c2
Pedro Vitor <pedro.santos22@escola.pr.gov.br>
2025-11-12 16:35:28 -0300

    teste 2

--- js/carrosel.js
+++ js/carrosel.js
@@ -103,7 +103,7 @@ const projetos = [// lista de imagesn
             href: "https://github.com/NadolnyJoao/ArcheType-Prot-tipos",
             text: "Acesar no Github"
         },
-        href: "pedrovitor-curriculo/game.html",
+        href: "/game.html",
         grupo: ""
     },
 ]
